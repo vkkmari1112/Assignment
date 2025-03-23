eb6b70091445afd9ab44febe1a1607fcc09c89db
Nanthini Kumari <nanthini.kumari11121991@gmail.com>
2025-03-23 21:16:12 +0530

    Assignment 1

--- src/Assignments/Assignments1_DataType.java
+++ src/Assignments/Assignments1_DataType.java
@@ -7,26 +7,32 @@ public class Assignments1_DataType {
 		
 		//1.Temperature of the city in degrees Celsius:25.5 
 		float a = 25.5f;
-		System.out.println(a);
-		
+		System.out.println("Temperature of the city in degrees Celsius:" +a);
 		
 		//2. Person's phone number: "9787129844"
 		Long phoneNumber =9787129844L;
-		System.out.println(+phoneNumber);
+		System.out.println("Person's phone number:"  +phoneNumber);
 		
 		//3. Amount of money in customer's bank account:1000.50
 		float accountBalance = 1000.50f;
-		System.out.println(+accountBalance);
+		System.out.println("Amount of money in customer's bank account:" +accountBalance);
 		
 		//4. Whether a Customer has placed an order : true or false
-		
+	    boolean myBoolean = true;
+	    System.out.println("Order is placed: " +myBoolean);
+	    
+	    boolean myBoolean2 = false;
+	    System.out.println("Order is not placed: " +myBoolean);
+	    
 		//5. Person email address:"nanthini.kumari111219911"
 		String mailAddress = "nanthini.kumari111219911@gmail.com";
-		System.out.println(mailAddress);
-		
+		System.out.println("Per on email address:"   +mailAddress);
+	
 		//6. Coordination of Location(latitude,longitude):37.7749,-122.4194
-		
-		
+		double latitude = 37.7749;
+		double longitude =  -122.4194;
+		System.out.println("My Location is:" +latitude  +longitude);
+
 	}
 
 }
