a2f4ce32563d74d2687675c8ca029005a3577b64
Nanthini Kumari <nanthini.kumari11121991@gmail.com>
2025-03-23 20:48:17 +0530

    Assignment 1

--- src/Assignments/Assignments1_DataType.java
+++ src/Assignments/Assignments1_DataType.java
@@ -2,9 +2,31 @@ package Assignments;
 
 public class Assignments1_DataType {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//1.Temperature of the city in degrees Celsius:25.5 
+		float a = 25.5f;
+		System.out.println(a);
+		
+		
+		//2. Person's phone number: "9787129844"
+		Long phoneNumber =9787129844L;
+		System.out.println(+phoneNumber);
+		
+		//3. Amount of money in customer's bank account:1000.50
+		float accountBalance = 1000.50f;
+		System.out.println(+accountBalance);
+		
+		//4. Whether a Customer has placed an order : true or false
+		
+		//5. Person email address:"nanthini.kumari111219911"
+		String mailAddress = "nanthini.kumari111219911@gmail.com";
+		System.out.println(mailAddress);
+		
+		//6. Coordination of Location(latitude,longitude):37.7749,-122.4194
+		
+		
 	}
 
 }
